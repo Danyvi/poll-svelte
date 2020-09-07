3e378eaab814c4ec6347deb377d79e452efc3671
Danyvi <30378170+Danyvi@users.noreply.github.com>
2020-09-06 23:05:38 -0400

    Poll Form Component

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
 <script>
 	import Header from "./components/Header.svelte";
 	import Footer from "./components/Footer.svelte";
+	import CreatePollForm from "./components/CreatePollForm.svelte";
 	import Tabs from "./shared/Tabs.svelte";
-
 	// Tabs
 	let items = ['Current Polls', 'Add New Poll'];
 	let activeItem = 'Current Polls';
@@ -19,7 +19,7 @@
 	{#if activeItem === 'Current Polls'}
 		<p>Poll List Component goes here</p>
 	{:else if activeItem === 'Add New Poll'}
-		<p>Add New Poll Component</p>
+		<CreatePollForm />
 	{/if}
 </main>
 <Footer />

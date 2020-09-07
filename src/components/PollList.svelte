ad7a75b44487ac802f1674058ce36c3e0f5361c4
Danyvi <30378170+Danyvi@users.noreply.github.com>
2020-09-07 12:10:57 -0400

    Lifecycle Hooks and auto sub-unsub from store

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,17 +1,36 @@
 <script>
+	// import { onMount, onDestroy } from 'svelte';
 	import PollStore from '../stores/PollStore.js';
 	import PollDetails from './PollDetails.svelte';
 	/* component that cycle through the data and adds a list of polls */
-	export let polls = [];
+	// export let polls = [];
 
-	// subscribe to the data inside the PollStore
-	PollStore.subscribe( data => {
-		polls = data;
-	});
+	// we can avoid the lifecycle hooks to subscribe or unsubscribe using
+	// $PollStore to reference directly to the store
+	// it will automatically unsubscribe from the store when the component is destroyed
+
+
+	// // subscribe to the data inside the PollStore
+	// const unsub = PollStore.subscribe( data => {
+	// 	polls = data;
+	// });
+
+	// onMount( () => {
+	// 	// good time to get data from a db
+	// 	console.log('component mounted');
+	// });
+
+	// onDestroy( () => {
+	// 	// good time to unsubscrive from a store
+	// 	// when a component is no longer needed/ destroyed
+	// 	console.log('component destroyed');
+	// 	unsub();
+	// });
+
 </script>
 
 <div class="poll-list">
-	{#each polls as poll (poll.id)}
+	{#each $PollStore as poll (poll.id)}
 		<div>
 			<PollDetails poll={poll} on:vote />
 		</div>

655df9c1cc07f29449fa6af8da9413f888c40984
Danyvi <30378170+Danyvi@users.noreply.github.com>
2020-09-06 22:39:07 -0400

    reusable tabs component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,26 @@
 <script>
 	import Header from "./components/Header.svelte";
-	import Footer from "./components/Footer.svelte"
+	import Footer from "./components/Footer.svelte";
+	import Tabs from "./shared/Tabs.svelte";
+
+	// Tabs
+	let items = ['Current Polls', 'Add New Poll'];
+	let activeItem = 'Current Polls';
+
+	const tabChange = (e)=>{
+		activeItem = e.detail; //item coming from Tabs component 
+	};
+
 </script>
 
 <Header />
 <main>
-	<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed voluptas magnam earum odit possimus tempore rerum dolore. Accusamus, sint voluptatum?</p>
+	<Tabs activeItem={activeItem} items={items} on:tabChange={tabChange}/>
+	{#if activeItem === 'Current Polls'}
+		<p>Poll List Component goes here</p>
+	{:else if activeItem === 'Add New Poll'}
+		<p>Add New Poll Component</p>
+	{/if}
 </main>
 <Footer />
 

43fc876819123e30afaab3a032914af2ebe93a3a
Danyvi <30378170+Danyvi@users.noreply.github.com>
2020-09-07 14:02:04 -0400

    add transition

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,37 +1,15 @@
 <script>
-	// import { onMount, onDestroy } from 'svelte';
+	import { fade, slide, scale } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
 	import PollStore from '../stores/PollStore.js';
 	import PollDetails from './PollDetails.svelte';
-	/* component that cycle through the data and adds a list of polls */
-	// export let polls = [];
-
-	// we can avoid the lifecycle hooks to subscribe or unsubscribe using
-	// $PollStore to reference directly to the store
-	// it will automatically unsubscribe from the store when the component is destroyed
-
-
-	// // subscribe to the data inside the PollStore
-	// const unsub = PollStore.subscribe( data => {
-	// 	polls = data;
-	// });
-
-	// onMount( () => {
-	// 	// good time to get data from a db
-	// 	console.log('component mounted');
-	// });
-
-	// onDestroy( () => {
-	// 	// good time to unsubscrive from a store
-	// 	// when a component is no longer needed/ destroyed
-	// 	console.log('component destroyed');
-	// 	unsub();
-	// });
-
+	// we can use trnsition only on HTML elements not on custom components
+	// that's the reason we use a div to wrap custom components
 </script>
 
 <div class="poll-list">
 	{#each $PollStore as poll (poll.id)}
-		<div>
+		<div in:fade out:scale|local animate:flip={{duration: 500}}>
 			<PollDetails poll={poll} />
 		</div>
 	{/each}
